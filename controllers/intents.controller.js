a62e027392f521a755d5e0e3def96260c310e315
Abdelaziz <abdelaziz.mezri@esprit.tn>
2022-06-15 17:00:25 +0200

    update and add sans controle de saisie

--- controllers/intents.controller.js
+++ controllers/intents.controller.js
@@ -13,14 +13,14 @@ var self = module.exports = {
     create: async(req, res) => {
         const { tag, patterns, responses } = req.body;
         const context_set = "";
-        console.log("console : " +tag +" , "+ patterns+" , "+ responses);
+        //console.log("console : " +tag +" , "+ patterns+" , "+ responses);
         const intentExist = await Intent.findOne({ tag: tag });
         if (intentExist) {
             return res.redirect('/intents?msg=Tag already used');
         }
         const arrayPatterns = patterns.split(";");
-        console.log(patterns)
-        console.log(arrayPatterns)
+        //console.log(patterns)
+        //console.log(arrayPatterns)
         const arrayResponses = responses.split(";");
         const intent = new Intent({
             tag,
@@ -49,21 +49,30 @@ var self = module.exports = {
         const { msg } = req.query || '';
         const { _id } = req.params;
         let intent = await Intent.findOne({ _id: _id });
-        res.render('intents/update', { intent, title: "Update Intent", account_id: req.session._id, username: req.session.username, msg });
+        let patterns = "";
+        let responses = "";
+        intent.patterns.forEach(pattern => {
+            patterns += pattern + ";";
+        });
+        intent.responses.forEach(response => {
+            responses += response + ";";
+        });
+        patterns = patterns.substring(0, patterns.length -1);
+        responses = responses.substring(0, responses.length -1);
+        res.render('intents/update', { intent, patterns, responses, title: "Update Intent", account_id: req.session._id, username: req.session.username, msg });
     },
     update: async(req, res) => {
         let { _id, tag, patterns, responses, context_set } = req.body;
-        const intent = await Intent.findOne({ _id: _id });
         const intentExist = await Intent.findOne({ tag: tag });
-        if (!intent) {
-            return res.status(400).json({ error: "Intent not found", _id: _id });
-        }
         if (intentExist) {
             if (intentExist._id != _id) {
                 return res.redirect('/intents?msg=Tag already used');
             }
         }
-        await Intent.updateOne({ _id: _id }, { tag: tag, patterns: patterns, responses: responses, context_set: context_set });
+        const arrayPatterns = patterns.split(";");
+        const arrayResponses = responses.split(";");
+        
+        await Intent.updateOne({ _id: _id }, { tag: tag, patterns: arrayPatterns, responses: arrayResponses, context_set: context_set });
         res.redirect('/intents?msg=Intents updated successfully');
     }
 }
