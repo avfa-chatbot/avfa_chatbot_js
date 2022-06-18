f7fe9f52b1fd45f37f6975d541ac71eae0a4b228
Abdelaziz Mezri <abdelaziz.mezri@esprit.tn>
2022-06-18 19:10:18 +0100

    change mongodb port

--- appParams.js
+++ appParams.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongodb_url: "mongodb://localhost:27017/avfa",
-    mongodb_url_docker: "mongodb://mongo:27017/avfa"
+    mongodb_url: "mongodb://localhost:27018/avfa",
+    mongodb_url_docker: "mongodb://mongo:27018/avfa"
 }
