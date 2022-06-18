c9dbf1584c52b80c221dbf4a069816a7b035dd6b
Abdelaziz Mezri <abdelaziz.mezri@esprit.tn>
2022-06-18 18:40:11 +0100

    mongodb docker-compose

--- appParams.js
+++ appParams.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongodb_url: "mongodb://localhost:27018/avfa",
-    mongodb_url_docker: "mongodb://mongo:27018/proximus"
+    mongodb_url_docker: "mongodb://mongo:27018/avfa"
 }
