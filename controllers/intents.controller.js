6b86ff984c54d6fe0d1bd9114b3611e448d75249
Abdelaziz Mezri <abdelaziz.mezri@esprit.tn>
2022-06-18 20:46:12 +0100

    can not delete intent if there is only one

--- controllers/intents.controller.js
+++ controllers/intents.controller.js
@@ -73,7 +73,7 @@ var self = module.exports = {
     delete: async(req, res) => {
         const { _id } = req.params;
 
-        const oneIntent = await Account.findOne();
+        const oneIntent = await Intent.findOne();
         if (!oneIntent) {
             return res.redirect('/intents?msg=You can not delete intent if there is only one');
         }
