86a1c48cfe57d46b32bfb97bd47fe55ad54c368a
Abdelaziz Mezri <abdelaziz.mezri@esprit.tn>
2022-06-23 09:11:44 +0100

    docker mode

--- python/static/app.js
+++ python/static/app.js
@@ -62,8 +62,8 @@ class Chatbox {
         redirect: 'follow'
         };
 
-        //fetch("http://vps-f7d0ecab.vps.ovh.net:5000/predict", requestOptions)
-        fetch("http://localhost:5000/predict", requestOptions)
+        fetch("http://vps-f7d0ecab.vps.ovh.net:5000/predict", requestOptions)
+        //fetch("http://localhost:5000/predict", requestOptions)
         .then(r => r.text())
         .then(r =>  {
                 const resp = JSON.parse(r);
